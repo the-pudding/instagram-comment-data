4dd9a68e6cf5cb20a4a623714fa577f4f422632d
russellgoldenberg <russellgoldenberg@gmail.com>
2019-07-23 15:07:10 -0400

    externalize tag var

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Scraping Instagram comments of a given hashtag.
 
 ## Installation
 `npm install`
+
+## Usage
+download **all** comments
+`npm start --tag=funny`
+
+download a small sample
+`npm start --tag=funny --test`
--- scripts/index.js
+++ scripts/index.js
@@ -3,45 +3,78 @@ const fs = require('fs');
 const rimraf = require('rimraf');
 const d3 = require('d3');
 
-const TAG = 'catsinboxes'
-const FILE_OUT = `./output/${TAG}.csv`;
-const TEST = false;
+let tag = null;
+let fileOut = null;
+let instaHash = null;
+let test = false;
+
+const TEST_TOTAL = 20;
 
 const OPTIONS = {
-	total: TEST ? 10 : 0,
+	total: TEST_TOTAL,
 	headless: true,
 	silent: true,
 	fullAPI: true,
 }
 
-const HASH = Instamancer.hashtag(TAG, OPTIONS);
+function printProgress(index) {
+	if (process.stdout.clearLine) process.stdout.clearLine();
+	process.stdout.cursorTo(0);
+	process.stdout.write(`posts scraped: ${index + 1}`);
+}
 
-function getComments({ edges = [], shortcode }) {
-	return edges.map(({ node }) => {
-		const { text, created_at } = node;
+function setup() {
+	tag = process.env.npm_config_tag || false;
+	test = !!process.env.npm_config_test;
+	fileOut = `./output/${tag}.csv`;
+	if (!test) OPTIONS.total = 0;
+	instaHash = Instamancer.hashtag(tag, OPTIONS);
+}
+
+
+function getComments({ edges = [], shortcode, id }) {
+	const comments = edges.map(({ node }) => {
+		const { text, created_at, owner } = node;
 		const clean = text.replace(/\n/g, ' ');
-		return { shortcode, created_at, text: clean };
-	});
+		return { shortcode, created_at, text: clean, id: owner.id };
+	}).filter(d => d.id !== id);
+
+	comments.forEach(d => delete d.id);
+	return comments;
 }
 
 async function init() {
-	rimraf.sync(FILE_OUT);
+	setup();
+	if (!tag) {
+		console.log('error: you need to pass a hashtag eg. "npm start --tag funny"');
+		return false;
+	} else {
+		const count = test ? TEST_TOTAL : 'all' ;
+		console.log(`scraping ${count} instagram posts for #${tag} ...sit tight.`);
+	}
+
+	rimraf.sync(fileOut);
 	const COLUMNS = ['shortcode', 'created_at', 'text']
-	fs.appendFileSync(FILE_OUT, `${d3.csvFormatBody([COLUMNS])}\n`); 
+	fs.appendFileSync(fileOut, `${d3.csvFormatBody([COLUMNS])}\n`); 
 
 	let i = 0;
-	for await (const post of HASH) {
-		console.log(i);
-		const { shortcode, edge_media_to_parent_comment } = post.shortcode_media;
-		const { edges } = edge_media_to_parent_comment;
-		const commentsTop = getComments({ edges, shortcode });
-		const commentsNested = edges.map(({ node }) => {
-			const threadEdges = node.edge_threaded_comments ? node.edge_threaded_comments.edges : []; 
-			return getComments({ edges: threadEdges, shortcode });
-		}).filter(d => d.length);
-		const commentsAll = commentsTop.concat(...commentsNested);
-		const chunk = `${d3.csvFormatBody(commentsAll)}\n`;
-		fs.appendFileSync(FILE_OUT, chunk);
+	for await (const post of instaHash) {
+		printProgress(i);
+		const { shortcode, edge_media_to_parent_comment, owner } = post.shortcode_media;
+		const { id } = owner;
+		if (edge_media_to_parent_comment) {
+			const { edges } = edge_media_to_parent_comment;
+			const top = getComments({ edges, shortcode, id });
+			const nested = edges.map(({ node }) => {
+				const threadEdges = node.edge_threaded_comments ? node.edge_threaded_comments.edges : [];
+				return getComments({ edges: threadEdges, shortcode, id });
+			}).filter(d => d.length);
+			const all = top.concat(...nested);
+			const clean = all.filter(d => d && d !== 'undefined' && d.text);
+			const formatted = d3.csvFormatBody(clean);
+			const chunk = `${formatted}\n`;
+			fs.appendFileSync(fileOut, chunk);
+		}
 		i += 1;
 	}
 }
